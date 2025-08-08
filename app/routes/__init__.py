7835eacd7b7aed03a875080052445a6214208054
cr625 <cr625@drexel.edu>
2025-08-08 10:56:53 -0400

    create experiment interface

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -3,5 +3,6 @@ from .auth import auth_bp
 from .text_input import text_input_bp
 from .processing import processing_bp
 from .results import results_bp
+from .experiments import experiments_bp
 
-__all__ = ['auth_bp', 'text_input_bp', 'processing_bp', 'results_bp']
+__all__ = ['auth_bp', 'text_input_bp', 'processing_bp', 'results_bp', 'experiments_bp']
